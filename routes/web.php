6fd4205bfb7cc762479ec86e75b9c56d5dd949e8
cungnguyen <cungnv2912@gmail.com>
2020-10-10 10:39:19 +0700

    get route home

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','App\Http\Controllers\HomeController@index');
 
-Route::get('/home', function () {
-    return view('welcome');
-});
+Route::get('/home', 'App\Http\Controllers\HomeController@index');
